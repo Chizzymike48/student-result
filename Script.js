570b918c18d80d668a17a0e7899740dcb4f9e7b3
Chizzymike48 <chizobam353@gmail.com>
2025-10-03 11:41:47 +0100

    corrections

--- Script.js
+++ Script.js
@@ -11,7 +11,7 @@
     let assignmentAverage = (assignment1 + assignment2) / 2;
 
     // Final grade (40% assignments, 60% exam)
-    let finalGrade = (assignmentAverage * 0.4) + (finalExam * 0.6);
+    let finalGrade = (assignmentAverage) + (finalExam * 0.6);
     console.log("finalGrade",finalGrade)
 
     let resultText = `Student: ${studentName}<br>Final Grade: ${finalGrade.toFixed(2)}<br>`;
